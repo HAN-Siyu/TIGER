f46406e07c3c9395d09f42a8dcc3b74de698a7b9
Siyu Han <alexh@alexh-MBP.local>
2021-01-28 22:00:48 +0100

    commit - test

---
+++ R/Internal.R
@@ -0,0 +1,33 @@
+compute_coefVar <- function(input_data) {
+    cv <- sd(input_data, na.rm = T) / mean(input_data, na.rm = T)
+    cv
+}
+
+.boxplot.stats <- function(x, coef = 1.5, do.conf = TRUE, do.out = TRUE) {
+    if (coef < 0)
+        stop("'coef' must not be negative")
+    nna <- !is.na(x)
+    n <- sum(nna)
+    stats <- stats::fivenum(x, na.rm = TRUE)
+    iqr <- diff(stats[c(2, 4)])
+    names(iqr) <- NULL
+    if (coef == 0)
+        do.out <- FALSE
+    else {
+        lower_limit <- (stats[2L] - coef * iqr)
+        upper_limit <- (stats[4L] + coef * iqr)
+        names(lower_limit) <- NULL
+        names(upper_limit) <- NULL
+        out <- if (!is.na(iqr)) {
+            x < lower_limit | x > upper_limit
+        }
+        else !is.finite(x)
+        if (any(out[nna], na.rm = TRUE))
+            stats[c(1, 5)] <- range(x[!out], na.rm = TRUE)
+    }
+    conf <- if (do.conf)
+        stats[3L] + c(-1.58, 1.58) * iqr/sqrt(n)
+    list(stats = stats, n = n, conf = conf,
+         out = if (do.out) x[out & nna] else numeric(),
+         iqr = iqr, lower_limit = lower_limit, upper_limit = upper_limit)
+}
